403ad5d2d83a82c9f63d83b0927b10464b6d890b
Bartek Pawlik <baxon.bobson@gmail.com>
2023-03-08 18:01:32 +0100

    full samll project

---
+++ app.js
@@ -0,0 +1,43 @@
+// using selectors inside the element
+
+const questions = document.querySelectorAll('.question-container')
+
+questions.forEach(function(question) {
+    // console.log(question)
+    const btn = question.querySelector('.btn')
+    // console.log(btn)
+   
+   
+   
+   
+   
+    btn.addEventListener('click', function(){
+
+        questions.forEach(function(item){
+     if(item !== question){
+        item.classList.remove("show-text")
+     }
+        })
+
+
+
+        question.classList.toggle('show-text')
+    })
+})
+
+//  traversing the dom 
+
+// const btns = document.querySelectorAll('.btn')
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//       const question =  e.currentTarget.parentElement.parentElement
+//       question.classList.toggle("show-text")
+//     })
+
+    
+    
+// })
+
+
+
